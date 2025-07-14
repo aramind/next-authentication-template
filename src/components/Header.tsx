93cbef65fede4163399a6f33e7aa6a52a5351a38
aramind <88814622+aramind@users.noreply.github.com>
2025-07-14 21:59:02 +0800

    styling: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+        {/* LOGO */}
+        <Link href="/" className="text-2xl font-bold">
+          LOGO
+        </Link>
+        {/* nav menus */}
+        <nav>
+          <ul className="flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/sign-in">Sign in</Link>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
